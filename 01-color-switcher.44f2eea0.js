59541b5fcc69f47b2dae54500e01b8fac127dd15
VargasAngie <112598704+VargasAngie@users.noreply.github.com>
2023-03-27 01:24:34 +0000

    Deploying to gh-pages from @ VargasAngie/goit-js-hw-09@4f2a8ea8847a04f0418f5c3c89176ecc1e90de62 🚀

---
+++ 01-color-switcher.44f2eea0.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");var a=null;e.addEventListener("click",(function(){e.disabled=!0,n.disabled=!1,a=setInterval((function(){t.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),n.addEventListener("click",(function(){e.disabled=!1,n.disabled=!0,clearInterval(a)}))}();
+//# sourceMappingURL=01-color-switcher.44f2eea0.js.map
